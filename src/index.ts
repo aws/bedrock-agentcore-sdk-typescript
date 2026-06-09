2c8f1d134c72f4852d34c015a68ded5325fb1830
xianzuyang9-blip <xianzuyang9@gmail.com>
2026-06-09 10:38:21 +0800

    fix: add package root entrypoint

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * TypeScript SDK for AWS Bedrock AgentCore.
+ */
+
+export * as identity from './identity/index.js'
+export * as runtime from './runtime/index.js'
+export * as browser from './tools/browser/index.js'
+export * as codeInterpreter from './tools/code-interpreter/index.js'
